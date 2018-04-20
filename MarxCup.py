c31e9dc53a739434c606117ae6d9ca88a1a26181
YYJeffrey <501934027@qq.com>
2018-04-20 12:55:13 +0800

    Update MarxCup.py
    新增了随机延迟以及自定义分数

--- MarxCup.py
+++ MarxCup.py
@@ -3,14 +3,20 @@
 # @Author  : Jeffrey
 import requests
 import json
+import random
+import time
 
 # login_url = 'https://www.qingsuyun.com/h5/107515/pc/exam/collection/#paperId=1803217582'
 answerId = input('请输入answerId：')
+score_str = input('请输入您想要的分数：')
+
 url = 'https://www.qingsuyun.com/h5/actions/exam/execute/find-exam.json'
 data = {'answerId': answerId, 'queryItems': True}
 r = requests.post(url, data=data)
 json_data = json.loads(r.text)['body']['examItems']
-# print(json_data)
+false_answer = []
+if (int(score_str) >= 0) and (int(score_str) <= 100):
+    false_answer = random.sample(range(1, 61), 100 - int(score_str))  # 随机错误答案，根据您输入的分数而定
 
 for j in range(80):
     questionId = json_data[j]['questionId']
@@ -18,17 +24,22 @@ for j in range(80):
         questions = json_data[j]['jsonData']['single']['options']
     else:
         questions = json_data[j]['jsonData']['multiple']['options']
-    # print(questions)
 
     answer = []
     for i in range(4):
-        if questions[i]['rightAnswers']:
-            answer.append(i)
+        if j in false_answer:
+            if not questions[i]['rightAnswers']:
+                answer.append(i)
+            break
+        else:
+            if questions[i]['rightAnswers']:
+                answer.append(i)
 
+    time.sleep(random.uniform(1, 3))    # 增加1-3秒的随机延迟防ban
     answer_url = 'https://www.qingsuyun.com/h5/actions/exam/execute/submit-answer.json'
     answer_data = {'answerId': answerId, 'questionId': questionId, 'answerContent': answer}
     result = requests.post(url=answer_url, data=answer_data)
-    print(result.text)
+    print(result.text + '已答完第{0}题'.format(j+1))
 
 result_url = 'https://www.qingsuyun.com/h5/actions/exam/execute/finish-exam.json'
 result_data = {'answerId': answerId, 'interrupt': False}
